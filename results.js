6123887a44c356f2a49275320c89859b02b7c054
Elise <eherrmannsfeldt@scu.edu>
2016-01-05 00:22:40 -0800

    not done (obvi). starting of results

---
+++ results.js
@@ -0,0 +1,15 @@
+var constructResults = function(results) {
+	
+	//results from search
+	if(results.module  === 'search') {
+		
+	}
+	//results from filter
+	else {
+		//
+		console.log(results.bbooks);
+		console.log(results.bquiz);
+		console.log(results.bvideos);
+		console.log(results.bgames);	
+	}
+}
