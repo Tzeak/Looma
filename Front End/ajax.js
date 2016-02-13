1075059926fc368e209456923bb996ef448c0f9a
tzeak <roshan@tzeak.com>
2016-02-12 17:22:44 -0800

    Comment ajax.js

--- Front End/ajax.js
+++ Front End/ajax.js
@@ -5,23 +5,26 @@
  */
 
 var timeline = function() {
-	var request = new XMLHttpRequest();			//Construct the ajax variable
+	var request = new XMLHttpRequest();		//Construct the ajax variable
 	var response;
 
-	req.onreadystatechange = function() {
-		if (req.readyState === 4)			//Upon receiving the response
-			  response = xhr.responseText;	//collect response
-					
 
+	request.onreadystatechange = function() {
+	/* This is a callback function. It is called after the connection is opened, request.send().
+	 * Everytime the request changes, this function is called due to "onreadystatechange".
+	 * When readyState === 4, that means a successful response has been collected
+	 * the response the Back End gives the Front End will be a STRING 
+	 * in JSON Format.
+	 */
+		if (request.readyState === 4)		//Upon receiving the response
+			response = xhr.responseText;	//collect response string
 
 	};
-	req.open("GET", "timeline.php"); //Or whatever we wind up calling the php module
+	request.open("GET", "function.php");	//Define the type of request (GET vs POST)
+	request.send(); //Connect to Server
+};
 
-	function sendAJAX() {
-		req.send(); //This is the sign that I still don't understand js - Roshan x.x
-	}
 
-}
 
 
 
