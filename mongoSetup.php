b697e328e3514222139734f339cebdcddd3a5302
tzeak <roshan@tzeak.com>
2016-02-25 22:31:40 -0800

    Add searchMongo function

--- mongoSetup.php
+++ mongoSetup.php
@@ -20,4 +20,24 @@ catch(MongoConnectionException $e)
 	echo "Error connecting to MongoDB. Make sure you have run the command mongod --dbpath data/";
 	exit();
 }
+
+function searchMongo($id)
+{
+
+	// Look through all collections for that document with the Mongo ID
+	$collectionarray = array($activities, $textbooks, $dictionary, $chapters);
+
+	for ($i=0; $i<count($collectionarray); $i++)
+   	{
+	
+		$document = $collectionarray[$i]->findOne(array('_id' => new MongoId($id)));
+
+		if ($document != null) 
+		{
+			return $document; // it's in this collection!
+		} 
+		
+
+	}
+}
 ?>
