ed98887de178a371a6649bac7e5b129d4afe65a8
katelk28 <katelk28@gmail.com>
2016-03-03 20:57:41 -0800

    Adding textbooks so we can practice the mongo pathnames

--- FrontEnd/index2.js
+++ FrontEnd/index2.js
@@ -523,13 +523,14 @@ console.log("query1: " + query);
 if (query.substring(0, 1) == '?') {
     query = query.substring(1);
 }
-var query2 = query.replace(/%22/g, '"');
-var query3 = query2.replace(/%20/g, " ");
-console.log("query3: " + query3);
-document.getElementById("displaybox").innerHTML = query3;
+query2 = decodeURI(query);
+// var query2 = query.replace(/%22/g, '"').replace(/%20/g, " ");
+// var query3 = query2;
+console.log("query2: " + query2);
+//document.getElementById("displaybox").innerHTML = query2;
 
 
-var resultArray = JSON.parse(query3);
+var resultArray = JSON.parse(query2);
 console.log("timeline: " + resultArray);
 
 var timelineDivs = document.getElementsByClassName("timelinediv");
