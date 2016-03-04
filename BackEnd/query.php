b7ef1faebc2b36798efb3bcbdc530daa3a6bd0aa
tzeak <roshan@tzeak.com>
2016-03-03 19:18:21 -0800

    clean query.php

--- BackEnd/query.php
+++ BackEnd/query.php
@@ -10,38 +10,42 @@
  					Grade, Subject, Chapter, Type of media
  					- Use a regex to search each 
 
- 	1. Grade
- 	2. Subject
- 	3. Chapter
- 	4. Type of media
- 	- Section (omitted)
+						1. Grade
+						2. Subject
+						3. Chapter
+						4 Section 
+						5. Type of media //In Progress
  */
 
 require_once('mongoSetup.php');
+
 //Construct Regular Expression for Filter
 $word = "";
 if(isset($_GET["grade"]) && $_GET["grade"] != '')
 {
-	$word .= $_GET["grade"]; // This should be of form ##EN####
+	$word .= $_GET["grade"]; 
 }
 else
 {
+	//Match any grade, 1-8
 	$word.= "[1-8]";
 }
 if(isset($_GET["subject"]) && $_GET["subject"] != '')
 {
-	$word .= $_GET["subject"]; // This should be of form ##EN####
+	$word .= $_GET["subject"]; 
 }
 else
 {
+	//Match any Subject of One or More Letters
 	$word .= "[A-Za-z]+";
 }
 if(isset($_GET["chapter"]) && $_GET["chapter"] != '')
 {
-	$word .= $_GET["chapter"]; // This should be of form ##EN####
+	$word .= $_GET["chapter"]; 
 }
 else
 {
+	//Match Any chapter from 0-99
 	$word.= "[0-9][0-9]?";
 }
 if(isset($_GET['section']) && $_GET["section"] != '')
@@ -57,9 +61,10 @@ else
 //echo "<br/><br/>".$word . "<br/><br/>";
 	
 	//Construct a query by placing regex into relevant array
-	$chapter_query = array('_id' => new MongoRegex("^$word/i"));  //NOTE: using regex to do a case insensitive search for the word 
-	$text_query= array('prefix' => new MongoRegex("^$word/i"));  //NOTE: using regex to do a case insensitive search for the word 
-	$else_query= array('ch_id' => new MongoRegex("^$word/i"));  //NOTE: using regex to do a case insensitive search for the word 
+	//NOTE: using regex to do a case insensitive search for the word 
+	$chapter_query = array('_id' => new MongoRegex("^$word/i"));  
+	$text_query= array('prefix' => new MongoRegex("^$word/i"));  
+	$else_query= array('ch_id' => new MongoRegex("^$word/i"));  
 
 	//Query Mongo Database
 	$res1 = queryMongo($else_query);	// Dictionary and Activities
@@ -71,7 +76,7 @@ else
 	echo($res3);
 
 
-	//Query Mongo Database
+//Query Mongo Database
 function queryMongo($searchArray) {
 	global $activities, $textbooks, $dictionary, $chapters, $timelines;
 
@@ -101,4 +106,5 @@ function queryMongo($searchArray) {
 	return $res;
 	
 }
+
 ?>
