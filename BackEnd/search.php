aa9e6307b3df38e25a531e38197a0c32d2306138
tzeak <roshan@tzeak.com>
2016-04-01 12:15:24 -0700

    Set basic search to output relevant files

--- BackEnd/search.php
+++ BackEnd/search.php
@@ -1,5 +1,12 @@
 <?php
-	echo exec("pwd");
+/*THIS IS NOT SANITIZED -- fix pls
+ * http://aseriesoftubes.com/articles/tutorial-web-interface-for-grep-and-cat-in-php/
+ * */
 	if(isset($_GET['search']) && $_GET['search'] != '')
-		echo exec("grep ". $_GET['search']);
+	{
+			echo escapeshellarg($_GET['search']) . "<br/>";
+			$results = shell_exec("find ". escapeshellarg($_GET['search'])."*");
+	}
+
+	//From results, search mongo for the respective file names
 ?>
