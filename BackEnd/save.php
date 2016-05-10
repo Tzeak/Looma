5dee5e0d29e81f4d5e2284380505aab7df38b4d8
Roshan <roshan@tzeak.com>
2016-05-10 18:12:50 -0400

    Break save

--- BackEnd/save.php
+++ BackEnd/save.php
@@ -13,19 +13,51 @@
  *				which will act as a repository for the open.html module. 
  */
 
-/*Connect to MongoDB*/
+//Connect to MongoDB//
 require_once 'mongoSetup.php';
-/*Set variables */
-$filename = 'timelines.json';
+//Set variables //
 
-/*Create and Insert Timeline into Database*/
+$filename = 'timelines.json';
+echo isset($_POST["timeline_id"]);
+//Open Timeline Repository
+$file = file_get_contents($filename);
+$timelineArray = json_decode($file);
+
+//CASE 1: Editing Existing Timeline//
+//if(isset($_POST["timeline_id"]))
+{
+/*
+	$timelineId = $_POST["timeline_id"];
+
+	//Find the mongo document for the relevant timeline
+	$info = timelines->findAndModify(
+		array("_id" => new MongoId($timelineId)),
+		array("$set" => array("name" => $_POST["lesson_title"], "line" => $_POST["items_array"])),
+	);
+	$info = fixDocId($info);
+
+	//Edit timeline in timeline.json
+	foreach($timelineArray as $key => $value) {
+		if (in_array($info["_id"], $value)) {
+			$timelineArray[$key] = $info;	// Removes from JSON
+			$timelineArray = array_values(array_filter($timelineArray));	// Resets indices once the item is removed
+			echo "Edited" . $info["name"];
+		}
+	}
+
+*/
+
+}
+/*
+else
+//CASE 2: Create and Insert new Timeline into Database//
+{
 	$info = array("name" => $_POST["lesson_title"], "line" => $_POST["items_array"]);
 	$timelines->insert($info);
 	$info = fixDocId($info);
-
-/*Open and Edit Timeline Repository*/
-	$file = file_get_contents($filename);
-	$timelineArray = json_decode($file);
+	
+	//Add new timeline information into $file//
 	$timelineArray[] = $info;
 	file_put_contents($filename, json_encode($timelineArray), LOCK_EX);
+}*/
 ?>
